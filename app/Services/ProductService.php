8e9edd50338e8df075c9509d1e9f9106960d309d
DIMAS AGUNG SETYOBUDI <agungdimas390@gmail.com>
2023-10-21 17:36:03 +0700

    Product service update

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -25,24 +25,24 @@ class ProductService
             'description' => $dataProduct['description'],
         ]);
         $path = 'products';
-        // $picture_default = $dataProduct['picture_default'];
-        // $file_name = $dataProduct['product_code'].'_default.png';
-        // $url = self::upploadFile($picture_default,$path,$file_name);
-        // $product->update([ 
-        //             'picture_default' => $url,
-        // ]);
-        // $pictures = $dataProduct['pictures'];
-        // if(!empty($pictures)){    
-        //     foreach ($pictures as $key => $value) {
-        //         $no = $key+1;
-        //         $file_name = $dataProduct['product_code'].'_'.$no.'.png';
-        //         $url = self::upploadFile($value,$path,$file_name);
-        //         $product->update([ 
-        //             'picture_'.$no => $url,
-        //         ]);
-        //         # code...
-        //     }
-        // }
+        $picture_default = $dataProduct['picture_default'];
+        $file_name = $dataProduct['product_code'].'_default.png';
+        $url = self::upploadFile($picture_default,$path,$file_name);
+        $product->update([ 
+                    'picture_default' => $url,
+        ]);
+        $pictures = $dataProduct['pictures'];
+        if(!empty($pictures)){    
+            foreach ($pictures as $key => $value) {
+                $no = $key+1;
+                $file_name = $dataProduct['product_code'].'_'.$no.'.png';
+                $url = self::upploadFile($value,$path,$file_name);
+                $product->update([ 
+                    'picture_'.$no => $url,
+                ]);
+                # code...
+            }
+        }
         $datavarian = [];
         $varian_detail_ids = [];
         foreach ($varians as $key => $value) {
@@ -59,7 +59,6 @@ class ProductService
             foreach ($varian_detail_ids[0] as $key => $value1) {
                 # code...
                 foreach ($varian_detail_ids[1] as $key => $value2) {
-                    # code...
                     $productVarians = ProductVarian::create([
                         'products_id' => $product->id,
                         'varian_detail_id_1' => $value1,
@@ -79,6 +78,77 @@ class ProductService
         
         
         
+        DB::commit();
+    }
+    function update($product,Array $dataProduct, Array $varians,Array $varian_details) {
+        DB::beginTransaction();
+        
+        $product->update([
+            'product_categories_id' => $dataProduct['product_categories_id'],
+            'product_code' => $dataProduct['product_code'],
+            'name' => $dataProduct['name'],
+            'length' => $dataProduct['length'],
+            'width' => $dataProduct['width'],
+            'height' => $dataProduct['height'],
+            'weight' => $dataProduct['weight'],
+            'order_type' => $dataProduct['weight'],
+            'description' => $dataProduct['description'],
+        ]);
+        $path = 'products';
+        $picture_default = $dataProduct['picture_default'];
+        $file_name = $dataProduct['product_code'].'_default.png';
+        $url = self::upploadFile($picture_default,$path,$file_name);
+        $product->update([ 
+                    'picture_default' => $url,
+        ]);
+        $pictures = $dataProduct['pictures'];
+        if(!empty($pictures)){    
+            foreach ($pictures as $key => $value) {
+                $no = $key+1;
+                $file_name = $dataProduct['product_code'].'_'.$no.'.png';
+                $url = self::upploadFile($value,$path,$file_name);
+                $product->update([ 
+                    'picture_'.$no => $url,
+                ]);
+                # code...
+            }
+        }
+        // delete varian
+        Varian::where('products_id'.$product->id)->delete();
+        VarianDetail::where('products_id'.$product->id)->delete();
+        ProductVarian::where('products_id'.$product->id)->delete();
+        $datavarian = [];
+        $varian_detail_ids = [];
+        foreach ($varians as $key => $value) {
+            $varian = Varian::create([
+                'products_id' => $product->id,
+                'name' => $value['name'],
+            ]);
+            $datavarian[] = $varian;
+            $varian_detail_ids[] = self::addVarianDetails($varian,$varian_details[$key]);
+        }
+        
+        if (count($varians) > 1) {
+            # code...
+            foreach ($varian_detail_ids[0] as $key => $value1) {
+                # code...
+                foreach ($varian_detail_ids[1] as $key => $value2) {
+                    $productVarians = ProductVarian::create([
+                        'products_id' => $product->id,
+                        'varian_detail_id_1' => $value1,
+                        'varian_detail_id_2' => $value2,
+                    ]);
+                }
+            }
+        }else{
+            foreach ($varian_detail_ids[0] as $key => $value1) {
+                $productVarians = ProductVarian::create([
+                    'products_id' => $product->id,
+                    'varian_detail_id_1' => $value1,
+                    'varian_detail_id_2' => null,
+                ]);
+            }
+        }
         DB::commit();
     }
     function addVarians($product_id,$varians){
