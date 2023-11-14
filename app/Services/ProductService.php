6710d59c65e43b5b3b33beecf44500879ac00292
DIMAS AGUNG SETYOBUDI <agungdimas390@gmail.com>
2023-11-14 15:32:51 +0700

    UPdate product

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -128,7 +128,9 @@ class ProductService
             }
         }
         // // delete varian
+        
         // $varianids = [];
+        $arrVarians =[];
         $lastVarian = Varian::where('products_id',$product->id)->get();
         foreach ($varians as $key => $varian) {
             foreach($lastVarian as $key=>$value){
@@ -138,67 +140,104 @@ class ProductService
                         'products_id' => $product->id,
                         'name' => $value['name'],
                     ]);
-                    $varianids[] = $varianNew;
+                    $varianids[] = $varianNew->id;
+                    $arrVarians[] = $varianNew;
                 }else{
                     
-                    $varianids[] = $value;
+                    $arrVarians[] = $value;
+                    $varianids[] = $value->id;
                 }
             }
         }
-        
+        // return $arrVarians[0];
         $varian_detail_ids = [];
         $lastVarianDetails = VarianDetail::whereIn('varians_id',$varianids)->get();
         foreach ($varian_details as $key1 => $varianD) {
-            $varianid = $varianids[$key]->id;
-            foreach($lastVarianDetails as $key=>$value){
-                if ($varianD != $value->name) {
-                    VarianDetail::where('id',$value->id)->delete();
-                    $varian_detail_ids[] = self::addVarianDetails($varianid,$varianD);
+            $varianid = $varianids[$key];
+            $arrVarian = $arrVarians[$key];
+            foreach ($varianD as $key => $value) {
+                # code...
+                // return $value;
+                $lastVarianDetails = VarianDetail::where('varians_id',$varianid)->where('name',$value)->first();
+                if (empty($lastVarianDetails)) {
+                    $varianDetail2 = [];
+                    $varianDetail2[]=$value;
+                    $varian_detail_ids[] = self::addVarianDetailsUpdate($arrVarian,$value);
+                    // $lastVarianDetails = VarianDetail::all();
                 }
-                $varian_detail_ids[] = $value->id;
+                $varian_detail_ids[] = $lastVarianDetails->id;
+                // return $lastVarianDetails;
             }
+
+            // var_dump($varianid);
+            // foreach($lastVarianDetails as $key=>$value){
+            //     if ($varianD != $value->name) {
+            //         // echo 123;
+            //         var_dump($varianD);
+            //         // echo $varianD;
+            //         VarianDetail::where('id',$value->id)->delete();
+            //         $varian_detail_ids[] = self::addVarianDetails($arrVarian,$varianD);
+            //     }
+            //     $varian_detail_ids[] = $value->id;
+            // }
+            VarianDetail::where('varians_id',$varianid)->whereNotIn('varians_id',$varian_detail_ids)->delete();
         }
+        // return $varian_detail_ids;
+        // return $lastVarianDetails;
         $lastVarian = Varian::where('products_id',$product->id)->get();
+        // return $lastVarian;
         $varian_details = [];
         foreach($lastVarian as $ke=>$val){
-            $varian_details[] =VarianDetail::where('varians_id',$value->id)->get();
+            $varian_details[] =VarianDetail::where('varians_id',$val->id)->get();
         }
+        // return $varian_details;
+        $product_varian_ids = [];
         // $lastVarianDetails = VarianDetail::whereIn('varians_id',$varianids)->get();
         if (count($lastVarian) > 1) {
             # code...
             foreach ($varian_details[0] as $key => $value1) {
+                // $lastVarianDetails = VarianDetail::whereIn('varians_id',$varianids)->get();
                 $lastProductVarians = ProductVarian::where('products_id',$product->id)->where('varian_detail_id_1',$value1->id)->get();
+                // return $lastVarian;
                 if (empty($lastProductVarians)) {
-                    ProductVarian::where('products_id',$product->id)->where('varian_detail_id_1',$value1->id)->delete();
-                }
-                foreach ($varian_details[1] as $key => $value2) {
-                    $checkProductVarian = ProductVarian::where('varian_detail_id_1',$value1->id)->where('varian_detail_id_2',$value2->id)->where('products_id',$product->id)->first();
-                    if (empty($checkProductVarian)) {
-                        # code...
-                        $productVarians = ProductVarian::create([
-                            'products_id' => $product->id,
-                            'varian_detail_id_1' => $value1->id,
-                            'varian_detail_id_2' => $value2->id,
-                        ]);
-                    }else{
-                        ProductVarian::where('varian_detail_id_1',$value1->id)->where('varian_detail_id_2',$value2->id)->where('products_id',$product->id)->delete();
+                    // ProductVarian::where('products_id',$product->id)->where('varian_detail_id_1',$value1->id)->delete();
+                    foreach ($varian_details[1] as $key => $value2) {
+                        $checkProductVarian = ProductVarian::where('varian_detail_id_1',$value1->id)->where('varian_detail_id_2',$value2->id)->where('products_id',$product->id)->first();
+                        if (empty($checkProductVarian)) {
+                            # code...
+                            ProductVarian::where('varian_detail_id_1',$value1->id)->where('varian_detail_id_2',$value2->id)->where('products_id',$product->id)->delete();
+                            $productVarians = ProductVarian::create([
+                                'products_id' => $product->id,
+                                'varian_detail_id_1' => $value1->id,
+                                'varian_detail_id_2' => $value2->id,
+                            ]);
+                            $product_varian_ids[] = $productVarians->id;
+                        }else{
+                            $product_varian_ids[] = $value2->id;
+                        }
                     }
+                }else{
+                    $product_varian_ids[] = $lastProductVarians->id;
                 }
             }
         }else{
             foreach ($varian_details[0] as $key => $value1) {
                 $checkProductVarian = ProductVarian::where('varian_detail_id_1',$value1->id)->where('products_id',$product->id)->first();
+                // return $checkProductVarian;
                 if (empty($checkProductVarian)) {
+                    // $checkProductVarian->delete();
                     $productVarians = ProductVarian::create([
                         'products_id' => $product->id,
                         'varian_detail_id_1' => $value1->id,
                         'varian_detail_id_2' => null,
                     ]);
+                    $product_varian_ids[] = $productVarians->id;
                 }else{
-                    $checkProductVarian->delete();
+                    $product_varian_ids[] = $checkProductVarian->id;
                 }
             }
         }
+        // return $product_varian_ids;
         // // VarianDetail::where('products_id',$product->id)->delete();
         // ProductVariran::where('products_id'.$product->id)->delete();
         // $datavarian = [];
@@ -256,6 +295,19 @@ class ProductService
             $varian_detail_ids[] = $varian_detail->id; 
         }
         return $varian_detail_ids;
+    }
+    function addVarianDetailsUpdate($varian,$details){
+        $varian_detail_ids = [];
+        // foreach ($details as $key => $value) {
+            # code...
+            $varian_detail = VarianDetail::create([
+                'varians_id' =>$varian->id,
+                'varians_name' =>$varian->name,
+                'name' => $details,
+            ]);
+            return $varian_detail; 
+        // }
+        // return $varian_detail_ids;
     }
     function checkProductDiscount($products_id,$qty) {
         $date = date('Y-m-d');
