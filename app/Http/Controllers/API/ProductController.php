f140ec8976e127c6517272800b7b7ed233e1fb54
DIMAS AGUNG <agungdimas390@gmail.com>
2024-01-24 13:33:30 +0700

    Add query is active in api product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -15,26 +15,26 @@ class ProductController extends Controller
         $product_categories_id = $request->input('product_categories_id');
         if ($product_categories_id) {
             # code...
-            $product= Product::with(['category','varians.detail','product_varian'])->where('product_categories_id',$product_categories_id)->latest()->get();
+            $product= Product::with(['category','varians.detail','product_varian'])->where('is_active',1)->where('product_categories_id',$product_categories_id)->latest()->get();
             foreach($product as $key=> $value){
                 $value->min_price = $value->product_varian->min('price');
                 $value->max_price = $value->product_varian->max('price');
-            }  
+            }
         }elseif ($products_id) {
             # code...
-            $product= Product::with(['category','varians.detail','product_varian'])->where('id',$products_id)->latest()->first();
-           
+            $product= Product::with(['category','varians.detail','product_varian'])->where('is_active',1)->where('id',$products_id)->latest()->first();
+
                 $product->min_price = $product->product_varian->min('price');
                 $product->max_price = $product->product_varian->max('price');
-            
+
         }else{
-            $product= Product::with(['category','varians.detail','product_varian'])->latest()->get();
+            $product= Product::with(['category','varians.detail','product_varian'])->where('is_active',1)->latest()->get();
             foreach($product as $key=> $value){
                 $value->min_price = $value->product_varian->min('price');
                 $value->max_price = $value->product_varian->max('price');
             }
         }
-        
+
         return $product;
     }
 
@@ -47,10 +47,10 @@ class ProductController extends Controller
             foreach($product as $key=> $value){
                 $value->min_price = $value->product_varian->min('price');
                 $value->max_price = $value->product_varian->max('price');
-            }  
+            }
         }
-        
-        
+
+
         return $product;
     }
 }
