98f5dda09019f5d7bc4f9a0685f29a10ab437a4f
Dimas Agung Setyobudi <agungdimas390@gmail.com>
2024-04-02 22:57:54 +0700

    add prefix no order

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -12,7 +12,7 @@ if (!function_exists('api_url_img')) {
 }
 if (!function_exists('generate_order_no')) {
     function generate_order_no($count) {
-        $prefix = date('Ymd');
+        $prefix = 'INV'.date('Ymd');
         $nomor_penomoran = $prefix . str_pad($count + 1, 3, '0', STR_PAD_LEFT);
         return $nomor_penomoran;
         // Logika fungsi Anda di sini
