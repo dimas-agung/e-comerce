286a1dcdd3f4d1b1500278625a69eae6afe011b3
Dimas Agung Setyobudi <agungdimas390@gmail.com>
2024-04-16 17:02:44 +0700

    fix series upload

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -152,9 +152,9 @@ class SeriesController extends Controller
         $fullPath = "{$path}/{$file_name}";
         // $img = Image::make('public/foo.jpg')->resize(320, 240)->insert('public/{$path}/{$file_name}');
         Storage::disk('public')->put($fullPath, $file_compressed);
-        $fullPath = "{$path}/{$file_name2}";
-        $img1 = Image::make($image)->resize(600, 700)->encode();
-        Storage::disk('public')->put($fullPath, $img1);
+        // $fullPath = "{$path}/{$file_name2}";
+        // $img1 = Image::make($image)->resize(600, 700)->encode();
+        // Storage::disk('public')->put($fullPath, $img1);
         // $files->storePubliclyAs($path, $file_name, "public");
         $url = $path.'/'. $file_name;
         return $url;
